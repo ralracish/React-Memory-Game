719c8aeff45997ccc557ed6f830e36baf24c7d87
ralracish <carlarblackman@gmail.com>
2019-04-29 17:15:08 -0400

    submission

--- memory/src/Components/Header/index.js
+++ memory/src/Components/Header/index.js
@@ -5,17 +5,20 @@ function Header(props) {
     return (
         <header className="header">
             <div className="row">
-                <div className="col-md-3">
+                <div className="col-md-12">
                     <h1 className="title"><span>Game of Thrones Clicky Game</span></h1>
                 </div>
-                <div className="col-md-3">
+            </div>
+            <div className="row">
+                <div className="col-md-3"></div>
+                <div className="col-md-5">
                     <h4 className="message"><span>{props.message}</span></h4>
                 </div>
-                <div className="col-md-3">
+                <div className="col-md-4">
                     <h5 className="score"><span>Score: {props.score}</span></h5>
-                    <h5 className="top-score"><span>Top Score: {props.topScore}</span></h5>
                 </div>
-            </div>    
+            </div>
+     
         </header>
     )
 }
--- memory/src/Components/Thrones/index.js
+++ memory/src/Components/Thrones/index.js
@@ -3,10 +3,10 @@ import "./style.css";
 
 function Thrones(props) {
     return (
-        <div className="jumbotron">
-        <div className="img-container col-md-3" onClick={() => props.z(props.id)}>
-            <img alt={props.x} src={props.y} height="150px" width="150px"/>
-        </div>
+        <div class="jumbotron">
+            <div className="img-container col-md-3" onClick={() => props.z(props.id)}>
+                <img alt={props.x} src={props.y} height="150px" width="150px"/>
+            </div>
         </div>
     );
 }
